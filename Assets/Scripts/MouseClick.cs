f483fd1101135096408dfc89a6b56b3f102445a2
Brian Payne <brianp.nz@gmail.com>
2018-09-23 19:30:00 +1200

    first commit

---
+++ Assets/Scripts/MouseClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Mouse was cliked on " + this.name);
+
+        BasicInkExample story = GetComponent<BasicInkExample>();
+
+        story.StartStory();
+    }
+}
